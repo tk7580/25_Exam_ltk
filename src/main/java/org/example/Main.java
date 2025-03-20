f712b46dec34d5cbd42861534caed9e7a0799eb4
tk7580 <tkcode7580@gmail.com>
2025-03-20 16:11:45 +0900

    no3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,27 +8,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int n = in.nextInt();
-
-        for (int i = n; i > 0; i--) {
-            if (n - i != 0){
-                for(int k = 0; k < n - i; k++){
-                    System.out.print(" ");
-                }
-            }
-            for (int j = i; j > 0; j--) {
-
-                System.out.print("");
-                if(j == i){
-                    System.out.print(" "+j);
-                }else if(j == n){
-                    System.out.print(j);
-                }else if(j == 1){
-                    System.out.println(" "+j);
-                }else{
-                    System.out.print(" "+j);
-                }
-            }
+        int [] arr = new int[11];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 1; i < 10; i++){
+            arr[i+1] = arr[i-1] + arr[i];
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
